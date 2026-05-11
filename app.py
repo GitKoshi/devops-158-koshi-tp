6c0891374812ca2b5404cde822f8c888aed6a389
Evan Fasoletti <fasoletievan@gmail.com>
2026-05-11 09:18:04 +0200

    Modification du message Test Lagona

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello EPSIC 158! - Modifié automatiquement par Jenkins !"
+    return "Hello EPSIC 158! - Test Lagona"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
